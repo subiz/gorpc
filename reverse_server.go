6b19df3cd3147e502cb932e128fe7e5bb456e02c
Thanh Pham Kieu <thanhpk@live.com>
2019-04-25 17:02:46 +0700

    add http handler

--- reverse_server.go
+++ reverse_server.go
@@ -1,16 +1,21 @@
 package gorpc
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"sync"
 	"sync/atomic"
-	"time"
+
+	"github.com/valyala/fasthttp"
 )
 
 type ReverseServer struct {
 	// Address to listen to for incoming connections.
-	Addr string
+	RPCAddr string
+
+	HttpPort int
 
 	// OnConnect is called whenever connection from client is accepted.
 	// The callback can be used for authentication/authorization/encryption
@@ -32,7 +37,7 @@ type ReverseServer struct {
 	// * NewUnixClient() and NewUnixServer() can be used for fast local
 	//   inter-process rpc.
 	//
-	// By default it returns TCP connections accepted from Server.Addr.
+	// By default it returns TCP connections accepted from Server.RPCAddr.
 	Listener Listener
 
 	// LogError is used for error logging.
@@ -46,110 +51,139 @@ type ReverseServer struct {
 	// any time you wish.
 	Stats ConnStats
 
-	serverStopChan chan struct{}
-	stopWg         sync.WaitGroup
+	requestcount uint64 // atomic, number of request sent, used to round robin
 
+	lock            *sync.Mutex // protect clients_by_path and clients
 	clients_by_path map[string][]*Client
 	clients         map[string]*Client // client by client addr
 }
 
-// Stop stops rpc server. Stopped server can be started again.
-func (me *ReverseServer) Stop() {
-	if me.serverStopChan == nil {
-		panic("gorpc.Server: server must be started before stopping it")
+// NewReverseServer creates a new ReverseServer object
+func NewReverseServer(rpc_addr string, http_port int) *ReverseServer {
+	return &ReverseServer{
+		HttpPort:        http_port,
+		RPCAddr:         rpc_addr,
+		lock:            &sync.Mutex{},
+		clients_by_path: make(map[string][]*Client),
+		clients:         make(map[string]*Client),
 	}
-	close(me.serverStopChan)
-	me.stopWg.Wait()
-	me.serverStopChan = nil
 }
 
 // Serve starts rpc server and blocks until it is stopped.
 func (me *ReverseServer) Serve() error {
-
 	if me.LogError == nil {
 		me.LogError = errorLogger
 	}
 
-	if me.serverStopChan != nil {
-		panic("gorpc.Server: server is already running. Stop it before starting it again")
-	}
-	me.serverStopChan = make(chan struct{})
-
 	if me.Listener == nil {
 		me.Listener = &defaultListener{}
 	}
-	if err := me.Listener.Init(me.Addr); err != nil {
-		err = fmt.Errorf("gorpc.Server: [%s]. Cannot listen to: [%s]", me.Addr, err)
+	if err := me.Listener.Init(me.RPCAddr); err != nil {
+		err = fmt.Errorf("gorpc.Server: [%s]. Cannot listen to: [%s]", me.RPCAddr, err)
 		me.LogError("%s", err)
 		return err
 	}
 
-	// workersCh := make(chan struct{}, s.Concurrency)
-	me.stopWg.Add(1)
 	workersCh := make(chan struct{}, 1000)
 	go me.serverHandler(workersCh)
-	me.stopWg.Wait()
-	return nil
+
+	return fasthttp.ListenAndServe(fmt.Sprintf(":%d", me.HttpPort), me.requestHandler)
+}
+
+var SLASH = []byte("/")
+
+func (me *ReverseServer) requestHandler(ctx *fasthttp.RequestCtx) {
+	firstpath := bytes.Split(ctx.Path(), SLASH)[0]
+	host := ctx.Host()
+
+	route := string(append(host, firstpath...))
+	count := int(atomic.AddUint64(&me.requestcount, 1))
+	me.lock.Lock()
+	clients := me.clients_by_path[route]
+	if len(clients) == 0 {
+		me.lock.Unlock()
+		ctx.Response.Header.SetStatusCode(404)
+		fmt.Fprintf(ctx, "not found any client %s", ctx.Path())
+		return
+	}
+	client := clients[count%len(clients)]
+	me.lock.Unlock()
+
+	cookies := make(map[string][]byte)
+	ctx.Request.Header.VisitAllCookie(func(key, val []byte) { cookies[string(key)] = val })
+	headers := make(map[string][]byte)
+	ctx.Request.Header.VisitAll(func(k, val []byte) {
+		key := string(bytes.ToLower(k))
+		if key == "content-type" || key == "cookie" || key == "user-agent" {
+			return
+		}
+		headers[key] = val
+	})
+	res, err := client.Call(Request{
+		Version:     "0.1",
+		Body:        ctx.Request.Body(),
+		Method:      ctx.Request.Header.Method(),
+		Uri:         ctx.Request.Header.RequestURI(),
+		ContentType: ctx.Request.Header.ContentType(),
+		UserAgent:   ctx.Request.Header.UserAgent(),
+		Cookies:     cookies,
+		Headers:     headers,
+	})
+
+	if err != nil {
+		ctx.Response.Header.SetStatusCode(500)
+		fmt.Fprintf(ctx, "internal err "+err.Error())
+		return
+	}
+	if res.Error != "" {
+		ctx.Response.Header.SetStatusCode(500)
+		fmt.Fprintf(ctx, "internal err "+res.Error)
+		return
+	}
+	ctx.Response.Header.SetStatusCode(int(res.StatusCode))
+	for k, v := range res.Headers {
+		ctx.Response.Header.SetBytesV(k, v)
+	}
+	ctx.Response.SetBody(res.Body)
 }
 
 func (me *ReverseServer) serverHandler(workersCh chan struct{}) {
-	defer me.stopWg.Done()
-
-	var conn io.ReadWriteCloser
-	var clientAddr string
-	var err error
-	var stopping atomic.Value
-
 	for {
-		acceptChan := make(chan struct{})
-		go func() {
-			if conn, clientAddr, err = me.Listener.Accept(); err != nil {
-				if stopping.Load() == nil {
-					me.LogError("gorpc.Server: [%s]. Cannot accept new connection: [%s]",
-						me.Addr, err)
-				}
-			}
-			close(acceptChan)
-		}()
-
-		select {
-		case <-me.serverStopChan:
-			stopping.Store(true)
-			me.Listener.Close()
-			<-acceptChan
-			return
-		case <-acceptChan:
-			me.Stats.incAcceptCalls()
-		}
+		conn, clientAddr, err := me.Listener.Accept()
 		if err != nil {
 			me.Stats.incAcceptErrors()
-			select {
-			case <-me.serverStopChan:
-				return
-			case <-time.After(time.Second):
-			}
-			continue
+			me.LogError("gorpc.Server: [%s]. Cannot accept new connection: [%s]",
+				me.RPCAddr, err)
 		}
-
-		me.stopWg.Add(1)
+		me.Stats.incAcceptCalls()
 		go me.newConnection(conn, clientAddr, workersCh)
 	}
 }
 
 func (me *ReverseServer) newConnection(conn io.ReadWriteCloser, clientAddr string, workersCh chan struct{}) {
-	defer me.stopWg.Done()
-
-	// var err error
-	// var stopping atomic.Value
-
 	client := &Client{
 		Addr: clientAddr,
 		Dial: func(_ string) (io.ReadWriteCloser, error) { return conn, nil },
 	}
 	client.Start()
-	//	clients_by_path
 	me.clients[clientAddr] = client
 
-	// client.Call()
+	response, err := client.Call(Request{Uri: []byte("_status")})
+	if err != nil {
+		me.LogError("gorpc.Server: [%s]. Error [%s]", me.RPCAddr, err)
+		conn.Close()
+	}
 
+	status := &StatusResponse{}
+	if err := json.Unmarshal(response.Body, status); err != nil {
+		me.LogError("gorpc.Server: [%s]. unable to get status [%s]", me.RPCAddr, err)
+		conn.Close()
+	}
+
+	me.lock.Lock()
+	for _, p := range status.GetPaths() {
+		clients := me.clients_by_path[p]
+		me.clients_by_path[p] = append(clients, client)
+	}
+	me.lock.Unlock()
 }
