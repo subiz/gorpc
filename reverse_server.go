10e56d3e216e19f684362550381e0924506097a1
Thanh Pham Kieu <thanhpk@live.com>
2019-04-25 21:33:41 +0700

    fix reverse server

--- reverse_server.go
+++ reverse_server.go
@@ -12,34 +12,6 @@ import (
 )
 
 type ReverseServer struct {
-	// Address to listen to for incoming connections.
-	RPCAddr string
-
-	HttpPort int
-
-	// OnConnect is called whenever connection from client is accepted.
-	// The callback can be used for authentication/authorization/encryption
-	// and/or for custom transport wrapping.
-	//
-	// See also Listener, which can be used for sophisticated transport
-	// implementation.
-	OnConnect OnConnectFunc
-
-	// The server obtains new client connections via Listener.Accept().
-	//
-	// Override the listener if you want custom underlying transport
-	// and/or client authentication/authorization.
-	// Don't forget overriding Client.Dial() callback accordingly.
-	//
-	// See also OnConnect for authentication/authorization purposes.
-	//
-	// * NewTLSClient() and NewTLSServer() can be used for encrypted rpc.
-	// * NewUnixClient() and NewUnixServer() can be used for fast local
-	//   inter-process rpc.
-	//
-	// By default it returns TCP connections accepted from Server.RPCAddr.
-	Listener Listener
-
 	// LogError is used for error logging.
 	//
 	// By default the function set via SetErrorLogger() is used.
@@ -59,10 +31,8 @@ type ReverseServer struct {
 }
 
 // NewReverseServer creates a new ReverseServer object
-func NewReverseServer(rpc_addr string, http_port int) *ReverseServer {
+func NewReverseServer() *ReverseServer {
 	return &ReverseServer{
-		HttpPort:        http_port,
-		RPCAddr:         rpc_addr,
 		lock:            &sync.Mutex{},
 		clients_by_path: make(map[string][]*Client),
 		clients:         make(map[string]*Client),
@@ -70,24 +40,29 @@ func NewReverseServer(rpc_addr string, http_port int) *ReverseServer {
 }
 
 // Serve starts rpc server and blocks until it is stopped.
-func (me *ReverseServer) Serve() error {
+func (me *ReverseServer) Serve(rpc_addr, http_addr string) {
+	go fasthttp.ListenAndServe(http_addr, me.requestHandler)
+
 	if me.LogError == nil {
 		me.LogError = errorLogger
 	}
 
-	if me.Listener == nil {
-		me.Listener = &defaultListener{}
-	}
-	if err := me.Listener.Init(me.RPCAddr); err != nil {
-		err = fmt.Errorf("gorpc.Server: [%s]. Cannot listen to: [%s]", me.RPCAddr, err)
-		me.LogError("%s", err)
-		return err
+	listener := &defaultListener{}
+	if err := listener.Init(rpc_addr); err != nil {
+		panic(err)
 	}
 
-	workersCh := make(chan struct{}, 1000)
-	go me.serverHandler(workersCh)
-
-	return fasthttp.ListenAndServe(fmt.Sprintf(":%d", me.HttpPort), me.requestHandler)
+	// the main tcp accept loop
+	for {
+		conn, clientAddr, err := listener.Accept()
+		if err != nil {
+			me.Stats.incAcceptErrors()
+			me.LogError("gorpc.Server: [%s]. Cannot accept new connection: [%s]", rpc_addr, err)
+			continue
+		}
+		me.Stats.incAcceptCalls()
+		go me.newConnection(conn, clientAddr)
+	}
 }
 
 var SLASH = []byte("/")
@@ -147,37 +122,53 @@ func (me *ReverseServer) requestHandler(ctx *fasthttp.RequestCtx) {
 	ctx.Response.SetBody(res.Body)
 }
 
-func (me *ReverseServer) serverHandler(workersCh chan struct{}) {
-	for {
-		conn, clientAddr, err := me.Listener.Accept()
-		if err != nil {
-			me.Stats.incAcceptErrors()
-			me.LogError("gorpc.Server: [%s]. Cannot accept new connection: [%s]",
-				me.RPCAddr, err)
-		}
-		me.Stats.incAcceptCalls()
-		go me.newConnection(conn, clientAddr, workersCh)
-	}
-}
-
-func (me *ReverseServer) newConnection(conn io.ReadWriteCloser, clientAddr string, workersCh chan struct{}) {
-	client := &Client{
+func (me *ReverseServer) newConnection(conn io.ReadWriteCloser, clientAddr string) {
+	var dialed bool
+	var client *Client
+	client = &Client{
 		Addr: clientAddr,
-		Dial: func(_ string) (io.ReadWriteCloser, error) { return conn, nil },
+		Dial: func(_ string) (io.ReadWriteCloser, error) {
+			if !dialed {
+				dialed = true
+				return conn, nil
+			}
+			// old connection is dead, we will not reuse the client
+			// because we are unable to reconnect to the NAT hided api server
+			go client.Stop()
+
+			// removing client from the routing map
+			me.lock.Lock()
+			for k, clients := range me.clients_by_path {
+				for i, c := range clients {
+					if c == client {
+						clients = append(clients[:i], clients[i+1:]...)
+					}
+				}
+				me.clients_by_path[k] = clients
+			}
+			me.lock.Unlock()
+			return nil, fmt.Errorf("STOPEED")
+		},
 	}
 	client.Start()
 	me.clients[clientAddr] = client
 
+	// first message is from the server to the endpoint
+	// endpoint should return paths that its able to handle
 	response, err := client.Call(Request{Uri: []byte("_status")})
 	if err != nil {
-		me.LogError("gorpc.Server: [%s]. Error [%s]", me.RPCAddr, err)
+		// oops, wrong protocol, or there is something wrong, cleaning
+		// me.LogError("gorpc.Server:. Error [%s]", err)
 		conn.Close()
+		return
 	}
 
 	status := &StatusResponse{}
 	if err := json.Unmarshal(response.Body, status); err != nil {
-		me.LogError("gorpc.Server: [%s]. unable to get status [%s]", me.RPCAddr, err)
+		// wrong answer
+		// me.LogError("gorpc.Server: unable to get status [%s]", err)
 		conn.Close()
+		return
 	}
 
 	me.lock.Lock()
