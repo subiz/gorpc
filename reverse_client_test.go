94b6d8fa49b4b98acc49f9dd8ccac4b861a079dc
Thanh Pham Kieu <thanhpk@live.com>
2019-04-26 14:46:43 +0700

    fix client test

--- reverse_client_test.go
+++ reverse_client_test.go
@@ -3,6 +3,7 @@ package gorpc
 import (
 	"encoding/json"
 	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -10,15 +11,18 @@ import (
 func TestReverseClient(t *testing.T) {
 
 	client := ReverseClient{ServerAddrs: []string{"127.0.0.1:1992"}}
+	count := uint64(0)
 	client.Start(func(clientAddr string, request Request) Response {
 		if string(request.Uri) == "_status" {
-			b, _ := json.Marshal(&StatusResponse{Paths: []string{"ukachi.com/van"}})
+			b, _ := json.Marshal(&StatusResponse{Paths: []string{"localhost:1995/hello"}})
 			println("GOT STATUS", string(b))
 
 			return Response{StatusCode: 200, Body: b}
 		}
-		fmt.Println("clientAddr", clientAddr, string(request.Method), string(request.Uri))
-		return Response{StatusCode: 200, Body: []byte("khoe khong")}
+
+		c := atomic.AddUint64(&count, 1)
+		fmt.Println(string(request.Method), string(request.Uri), "count", c)
+		return Response{StatusCode: 200, Body: []byte("khoe khong--" + time.Now().String())}
 	})
 	time.Sleep(100 * time.Hour)
 }
