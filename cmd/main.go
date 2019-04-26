81bf33ee75bd021fcb0e35d4af1fd01651e33b89
Thanh Pham Kieu <thanhpk@live.com>
2019-04-26 19:16:11 +0700

    fix cmd param

--- cmd/main.go
+++ cmd/main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	fmt.Println("REVERSE PROXY SERVER v0.5")
-	var rpc_addr = *flag.String("rpc", ":5000", "address for the RPC server")
-	var http_addr = *flag.String("http", ":80", "address for the Http server")
+	var rpc_addr = flag.String("rpc", ":5000", "address for the RPC server")
+	var http_addr = flag.String("http", ":80", "address for the Http server")
 	flag.Parse()
 
 	server := gorpc.NewReverseServer()
-	server.Serve(rpc_addr, http_addr)
+	server.Serve(*rpc_addr, *http_addr)
 }
