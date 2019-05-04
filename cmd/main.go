e4f153e99917cd9a9572b11be43f86f5c182ad0c
Thanh Pham Kieu <thanhpk@live.com>
2019-05-04 19:52:56 +0700

    release v0.6.1

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("REVERSE PROXY SERVER v0.5")
+	fmt.Println("REVERSE PROXY SERVER v0.6.1")
 	var rpc_addr = flag.String("rpc", ":5000", "address for the RPC server")
 	var http_addr = flag.String("http", ":80", "address for the Http server")
 	flag.Parse()
