da655f386fad6b4bc7d02fd7ccd779e80db92c82
Thanh Pham Kieu <thanhpk@live.com>
2019-05-09 15:28:20 +0700

    make helper even easier to use

--- worker_helper.go
+++ worker_helper.go
@@ -25,7 +25,7 @@ func NewRouter(proxy_addrs, domains []string) *Router {
 		post_root:   &node{},
 		del_root:    &node{},
 		def: func(c *Context) {
-			c.String(400, "dashboard not found :(( "+c.Request.Path+".")
+			c.String(400, "dashboard not found :(( "+c.request.Path+".")
 		},
 		stop: make(chan bool),
 	}
@@ -91,31 +91,18 @@ func (me *Router) Handle(req Request) (res Response) {
 		root = me.del_root
 	}
 
-	if req.Header == nil {
-		req.Header = make(map[string][]byte)
-	}
-	if req.Query == nil {
-		req.Query = make(map[string][]byte)
-	}
-	if req.Form == nil {
-		req.Form = make(map[string][]byte)
-	}
-	if req.Cookie == nil {
-		req.Cookie = make(map[string][]byte)
-	}
-	res.Header = make(map[string][]byte)
 	if root == nil {
-		me.def(&Context{Request: req, Params: Params{}, Store: make(map[string]string), Response: &res})
+		me.def(&Context{request: req, params: Params{}, response: &res})
 		return res
 	}
 	h, ps, _ := root.getValue(req.Path)
 	handler, _ := h.(Handler)
 	if handler == nil {
-		me.def(&Context{Request: req, Params: ps, Store: make(map[string]string), Response: &res})
+		me.def(&Context{request: req, params: ps, response: &res})
 		return res
 	}
 
-	handler(&Context{Request: req, Params: ps, Store: make(map[string]string), Response: &res})
+	handler(&Context{request: req, params: ps, response: &res})
 	return res
 }
 
@@ -150,7 +137,7 @@ func (me *Router) wraps(handles ...Handler) Handler {
 	return func(c *Context) {
 		for _, h := range handles {
 			h(c)
-			if c.Aborted {
+			if c.aborted {
 				break
 			}
 		}
@@ -159,6 +146,8 @@ func (me *Router) wraps(handles ...Handler) Handler {
 
 var TEXTPLAIN = []byte("text/plain")
 var APPJSON = []byte("application/json")
+var JAVASCRIPT = []byte("text/javascript")
+var TEXTHTML = []byte("text/html")
 
 type Marshaller interface {
 	MarshalJSON() ([]byte, error)
@@ -169,42 +158,154 @@ type Unmarshaller interface {
 }
 
 type Context struct {
-	Request  Request
-	Params   Params
-	Store    map[string]string
-	Response *Response
-	Aborted  bool
+	request  Request
+	params   Params
+	store    map[string]string
+	response *Response
+	aborted  bool
 }
 
 func (c *Context) JSON(code int, v interface{}) {
-	c.Response.StatusCode = int32(code)
-	c.Response.Header["content-type"] = APPJSON
+	c.response.StatusCode = int32(code)
+	c.SetHeader("content-type", APPJSON)
 
 	if mars, ok := v.(Marshaller); ok {
-		c.Response.Body, _ = mars.MarshalJSON()
-	} else {
-		c.Response.Body, _ = json.Marshal(v)
+		c.response.Body, _ = mars.MarshalJSON()
+		return
 	}
+	c.response.Body, _ = json.Marshal(v)
 }
 
 func (c *Context) Data(code int, contenttype string, data []byte) {
-	c.Response.StatusCode = int32(code)
-	c.Response.Header["content-type"] = []byte(contenttype)
-	c.Response.Body = data
+	c.response.StatusCode = int32(code)
+
+	var ct []byte = nil
+	switch contenttype {
+	case "text/plain":
+		ct = TEXTPLAIN
+	case "application/json":
+		ct = APPJSON
+	case "text/javascript":
+		ct = JAVASCRIPT
+	case "text/html":
+		ct = TEXTHTML
+	default:
+		ct = []byte(contenttype)
+	}
+
+	c.SetHeader("content-type", ct)
+	c.response.Body = data
 }
 
 func (c *Context) String(code int, str string) {
-	c.Response.StatusCode = int32(code)
-	c.Response.Header["content-type"] = TEXTPLAIN
-	c.Response.Body = []byte(str)
+	c.response.StatusCode = int32(code)
+	c.SetHeader("content-type", TEXTPLAIN)
+	c.response.Body = []byte(str)
 }
 
-func (c *Context) Abort() {
-	c.Aborted = true
+func (c *Context) Abort() { c.aborted = true }
+
+func (c *Context) SetString(key, val string) {
+	if c.store == nil {
+		c.store = make(map[string]string)
+	}
+	c.store[key] = val
 }
 
-func (c *Context) SetCode(statuscode int) {
-	c.Response.StatusCode = int32(statuscode)
+func (c *Context) GetString(key string) string {
+	if c.store == nil {
+		return ""
+	}
+	return c.store[key]
+}
+
+func (c *Context) Status(statuscode int) {
+	c.response.StatusCode = int32(statuscode)
+}
+
+func (c *Context) Params(name string) string {
+	if c.params == nil {
+		return ""
+	}
+
+	return c.params[name]
+}
+
+func (c *Context) PostForm(name string) string {
+	if c.request.Form == nil {
+		return ""
+	}
+
+	return c.request.Form[name]
+}
+
+func (c *Context) Query(name string) string {
+	if c.request.Query == nil {
+		return ""
+	}
+
+	return c.request.Query[name]
+}
+
+// Cookie lookups cookie by key from the request
+func (c *Context) Cookie(key string) string {
+	if c.request.Cookie == nil {
+		return ""
+	}
+
+	return string(c.request.Cookie[key])
+}
+
+// AddCookie add cookie header to response
+func (c *Context) AddCookie(cook *Cookie) {
+	c.response.Cookies = append(c.response.Cookies, cook)
+}
+
+// SetHeader adds a header to response, override last header with same key
+func (c *Context) SetHeader(key string, val []byte) {
+	if c.response.Header == nil {
+		c.response.Header = make(map[string][]byte)
+	}
+	c.response.Header[key] = val
+}
+
+// SetHeader adds a header to response, override last header with same key
+func (c *Context) Header(key string) []byte {
+	if c.request.Header == nil {
+		return nil
+	}
+	return c.request.Header[key]
+}
+
+func (c *Context) Method() string {
+	return c.request.Method
+}
+
+func (c *Context) Uri() string {
+	return string(c.request.Uri)
+}
+
+func (c *Context) Body() []byte {
+	return c.request.Body
+}
+
+func (c *Context) UserAgent() []byte {
+	return c.request.UserAgent
+}
+
+func (c *Context) RemoteAddr() string {
+	return c.request.RemoteAddr
+}
+func (c *Context) Referer() string {
+	return c.request.Referer
+}
+
+func (c *Context) Path() string {
+	return c.request.Path
+}
+
+func (c *Context) Host() string {
+	return c.request.Host
 }
 
 type H map[string]interface{}
